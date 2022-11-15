11b24c25e020b64f680b55ce3038535c1dc070ba
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2022-11-15 18:32:39 +0100

    global amount rounded to 2 decimals

--- routes/payment.js
+++ routes/payment.js
@@ -31,7 +31,7 @@ router.post("/payment", isAuthenticated, async (req, res) => {
 
     // realisation du paiment
     const response = await stripe.charges.create({
-      amount: globalAmount * 100,
+      amount: globalAmount.toFixed(2) * 100,
       currency: "eur",
       description: offer.product_name,
       source: stripeToken,
